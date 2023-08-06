83f462bcdf68d4a3bb6e91d51f39cf17c0b1d422
epicgdog <theepicgdog@gmail.com>
2023-08-05 17:04:50 -0700

    starting over

--- src/football/urls.py
+++
@@ -1,9 +0,0 @@
-# path is a function that connects an HTTP endpoint to a view
-from django.urls import path
-
-from . import views
-
-# different HTTP endpoints and what view they are connected to
-urlpatterns = [
-    path("", views.index, name="index"),
-]
